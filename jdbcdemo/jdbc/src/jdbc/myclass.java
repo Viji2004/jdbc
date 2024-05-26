73694cde72f8f7343076cff024ab0d41dd47b561
Viji S <99385231+Viji2004@users.noreply.github.com>
2024-05-26 11:01:52 +0530

    Add files via upload

---
+++ jdbcdemo/jdbc/src/jdbc/myclass.java
@@ -0,0 +1,19 @@
+package jdbc;
+import java.sql.*;
+public class myclass {
+public static void main(String args[]) throws Exception{
+	String url="jdbc:mysql://localhost:3306/mydb";
+	String username="root";
+	String password="1234";
+	String query="select name from xyz where id=2";
+Class.forName("com.mysql.jdbc.Driver");
+Connection con=DriverManager.getConnection(url,username,password);
+Statement st=con.createStatement();
+ResultSet rs=st.executeQuery(query);
+rs.next();
+String name=rs.getString("name");
+System.out.println(name);
+st.close();
+con.close();
+}
+}
